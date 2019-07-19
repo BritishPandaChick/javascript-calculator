ffc3d33c88310857bac10a558953bababca9ed1b
BritishPandaChick <sbartley1080@gmail.com>
2019-07-18 20:12:32 -0400

    Update script file

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
   var operators2 = ["."];  //numbers for validation
   var nums = [0,1,2,3,4,5,6,7,8,8];  //Adds value to inputs array and checks validation
 
-  function getValue(input){    if(operators2.includes(inputs[inputs.length-1])===true && input===".")){
+  function getValue(input){
+    if(operators2.includes(inputs[inputs.length-1])===true && input===".")){
     console.log("Duplicate '.'");
   }
 
