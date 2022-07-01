b56d32c464f4f0427804f13d0ac0d2055af836cf
BritishPandaChick <sbartley1080@gmail.com>
2022-07-01 00:15:55 -0400

    Add functions for displaying numbers and decimal. Create function to get equal sign to work

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,9 +4,41 @@ import './App.css';
 function App() {
   const [display, setDisplay] = useState('0');
 
-  const handleNumber = () => {
+  // Change all numbers
+  const handleNumber = (event) => {
+    const number = event.target.textContent;
 
-  }
+    //Adds onto the number instead of replacing number in display
+    if (display === '0') {
+      setDisplay(number);
+    } else {
+      setDisplay(display + number);
+    }
+  };
+
+  // Gets operators to work on the display
+  const handleOperator = (event) => {
+    const operator = event.target.textContent;
+    setDisplay(display + ' ' + operator + ' ');
+  };
+
+  // Gets equal sign to work 
+  const handleEqual = () => {
+    //Evaluate numbers in the display
+    setDisplay(eval(display));
+  };
+
+  // Gets decimal to work
+  const handleDecimal = () => {
+    const array = display.split(' ');
+    const lastElement = array[array.length - 1];
+
+    //Checks last number to see if it includes a decimal.
+    if (!lastElement.includes('.') &&
+      (typeof parseInt(lastElement) === 'number')) {
+      setDisplay(display + '.');
+    }
+  };
 
   return (
     <div className="App">
@@ -17,22 +49,69 @@ function App() {
         <div id="clear" className="row">
           AC
         </div>
-        <div id="seven" onClick={handleNumber}>7</div>
-        <div id="eight" onClick={handleNumber}>8</div>
-        <div id="nine" onClick={handleNumber}>9</div>
-        <div id="multiply">*</div>
-        <div id="four" onClick={handleNumber}>4</div>
-        <div id="five" onClick={handleNumber}>5</div>
-        <div id="six" onClick={handleNumber}>6</div>
-        <div id="divide">/</div>
-        <div id="one" onClick={handleNumber}>1</div>
-        <div id="two" onClick={handleNumber}>2</div>
-        <div id="three" onClick={handleNumber}>3</div>
-        <div id="add">+</div>
-        <div id="zero" onClick={handleNumber}>0</div>
-        <div id="decimal">.</div>
-        <div id="equals">=</div>
-        <div id="subtract">-</div>
+
+        <div id="seven" onClick={handleNumber}>
+          7
+        </div>
+
+        <div id="eight" onClick={handleNumber}>
+          8
+        </div>
+
+        <div id="nine" onClick={handleNumber}>
+          9
+        </div>
+
+        <div id="multiply" onClick={handleOperator}>
+          *
+        </div>
+
+        <div id="four" onClick={handleNumber}>
+          4
+        </div>
+
+        <div id="five" onClick={handleNumber}>
+          5
+        </div>
+
+        <div id="six" onClick={handleNumber}>
+          6
+        </div>
+
+        <div id="divide" onClick={handleOperator}>
+          /
+        </div>
+
+        <div id="one" onClick={handleNumber}>
+          1
+        </div>
+
+        <div id="two" onClick={handleNumber}>
+          2
+        </div>
+
+        <div id="three" onClick={handleNumber}>
+          3
+        </div>
+
+        <div id="add" onClick={handleOperator}>
+          +
+        </div>
+
+        <div id="zero" onClick={handleNumber}>
+          0
+        </div>
+
+        <div id="decimal" onClick={handleDecimal}>
+          .
+        </div>
+        <div id="equals" onClick={handleEqual}>
+          =
+        </div>
+
+        <div id="subtract" onClick={handleOperator}>
+          -
+        </div>
       </div>
     </div>
   );
